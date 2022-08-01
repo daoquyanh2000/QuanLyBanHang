ce27ccecfd654282edb7387ae4877631d5905ba1
daoquyanh2000 <daoquyanh2000@gmail.com>
2022-08-02 03:22:14 +0700

    checkout

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,57 @@
+﻿using QuanLyBanHang.DB;
+using QuanLyBanHang.DB.Entities;
+using QuanLyBanHang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyBanHang.Controllers
+{
+
+    public class CheckoutController : Controller
+    {
+        private const string CartSession = "CartSession";
+        private StoreContext db = new StoreContext();
+
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Customer  customer)
+        {
+            var listItem = (List<CartItem>)Session[CartSession];
+            var total = listItem.Sum(x => x.Quantity * x.Price);
+            var orderDetails = new List<OrderDetail>();
+
+            var order = new Order()
+            {
+                CreatedTime = DateTime.Now,
+                Status = OrderStatus.Pending,
+                Total = (long)total,
+                Customer=customer,
+            };
+            foreach (var item in listItem)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    Quantity = item.Quantity,
+                    PricePerUnit = (long)item.Price,
+                    Money = total,
+                };
+                orderDetails.Add(orderDetail);
+            };
+            order.OrderDetails = orderDetails;
+
+            db.Orders.Add(order);
+
+            db.SaveChanges();
+
+
+            return View();
+        }
+    }
+}
--- DB/Entities/Order.cs
+++ DB/Entities/Order.cs
@@ -12,13 +12,24 @@ namespace QuanLyBanHang.DB.Entities
     {
         public int Id { get; set; }
         public DateTime CreatedTime { get; set; }
-        public int Status { get; set; }
+        public OrderStatus Status { get; set; }
         [ForeignKey("Customer")]
         public int CustomerId { get; set; }
         public virtual Customer Customer { get; set; }
         public virtual DeliveryEmployee DeliveryEmployee { get; set; }
-        public virtual List<OrderDetail> OrderDetails { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
         public long Total { get; set; }
 
+    }
+    public enum OrderStatus
+    {
+        [Display(Name = "Chờ xác nhận")]
+        Pending=0,
+        [Display(Name = "Đang giao hàng")]
+        Shipping = 1,
+        [Display(Name = "Đã giao hàng")]
+        Shipped = 2,
+        [Display(Name = "Đã hủy")]
+        Cancel = 10,
     }
 }
